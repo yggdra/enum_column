517c17620a9dd169115d822a1162c57e8a347c1e
Daniele Orlandi <daniele@orlandi.com>
2012-01-16 14:12:18 +0100

    Separated mysql and mysql2 adapters

--- lib/enum_column.rb
+++ lib/enum_column.rb
@@ -3,20 +3,9 @@ if defined?(::Rails::Railtie)
     initializer 'enum_column.initialize', :after => 'active_record.initialize_database' do |app|
       ActiveSupport.on_load :active_record do
 
-        if defined? ActiveRecord::ConnectionAdapters::MySQLJdbcConnection ||
-           defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter ||
-           defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
-          require 'enum/mysql/adapter'
-          require 'enum/mysql/enum_adapter'
-          require 'enum/mysql/schema_statements'
-          require 'enum/mysql/schema_definitions'
-        end
-
-        if defined? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
-          require 'enum/pgsql/adapter'
-          require 'enum/pgsql/enum_adapter'
-          require 'enum/pgsql/schema_statements'
-        end
+        require 'enum/mysql'
+        require 'enum/mysql2'
+        require 'enum/pgsql'
 
         require 'enum/quoting'
         require 'enum/validations'
