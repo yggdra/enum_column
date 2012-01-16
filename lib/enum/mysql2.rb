d77b0c64ed369b32e45a8cbdc11fdf8dbc16fd75
Daniele Orlandi <daniele@orlandi.com>
2012-01-16 14:21:53 +0100

    Fixed require path

--- lib/enum/mysql2.rb
+++ lib/enum/mysql2.rb
@@ -1,6 +1,6 @@
 if defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  require 'mysql2/adapter'
-  require 'mysql2/enum_adapter'
-  require 'mysql2/schema_statements'
-  require 'mysql2/schema_definitions'
+  require 'enum/mysql2/adapter'
+  require 'enum/mysql2/enum_adapter'
+  require 'enum/mysql2/schema_statements'
+  require 'enum/mysql2/schema_definitions'
 end
