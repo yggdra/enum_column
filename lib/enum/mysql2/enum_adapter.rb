0e2934ab93696efa7234aa7b630ecca66b0afc68
Daniele Orlandi <daniele@orlandi.com>
2012-01-17 10:27:16 +0100

    Ported mysql2 to rails 3.2 version

--- lib/enum/mysql2/enum_adapter.rb
+++ lib/enum/mysql2/enum_adapter.rb
@@ -2,7 +2,7 @@
 # This module provides all the column helper methods to deal with the
 # values and adds the common type management code for the adapters.
 
-ActiveRecord::ConnectionAdapters::Mysql2Column.module_eval do
+ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column.module_eval do
   alias __klass_enum klass
   # The class for enum is Symbol.
   def klass
